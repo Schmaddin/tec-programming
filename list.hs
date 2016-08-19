4b54ea8a68cea3cc3e0dd3925b0f18f7fd43bd55
Schmaddin <Martin.wuerflein@gmx.de>
2016-08-19 08:29:00 +0200

    deberes por el 19.08
    Signed-off-by: Schmaddin <Martin.wuerflein@gmx.de>

---
+++ list.hs
@@ -0,0 +1,38 @@
+bigger :: (Double,Double) -> Double
+bigger(a,b) = if a > b 
+			  then a
+			  else b
+smaller :: (Double,Double) -> Double
+smaller(a,b) = if a < b 
+			   then a
+			   else b
+
+maxi :: [Double] -> Double
+maxi [] = -1
+maxi (x:[])=x
+maxi (a:b:c) = if c == []
+			  then bigger(a,b)
+			  else maxi([bigger(a,b)]++c)
+
+mini :: [Double] -> Double
+mini [] = -1
+mini (x:[])=x
+mini (a:b:c) = if c == []
+			  then smaller(a,b)
+			  else mini([smaller(a,b)]++c)
+
+maxmin :: [Double] -> (Double,Double)
+maxmin list = (mini list,maxi list)	
+
+
+
+helpMaxTuples :: ([[Double]],[(Double,Double)]) -> ([[Double]],[(Double,Double)])
+helpMaxTuples(a:as,b) = helpMaxTuples(as,b++[maxmin a])
+helpMaxTuples([],b) = ([],b)
+
+tupleExtractor :: ([[Double]],[(Double,Double)]) -> [(Double,Double)]
+tupleExtractor(a,b) = b
+
+maxTuples :: [[Double]] -> [(Double,Double)]
+maxTuples list = tupleExtractor(helpMaxTuples(list,[]))
+
