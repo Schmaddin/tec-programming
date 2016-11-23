214f4ab44804e110bebf40324fb6f7b8c459eea7
Schmaddin <Martin.wuerflein@gmx.de>
2016-11-23 06:43:12 +0100

    Add files via upload
    insertB for balanced insert
    deleteB for balanced deletion

--- avl.pl
+++ avl.pl
@@ -4,11 +4,24 @@ insert(Num,empty,tree(Num,empty,empty)).
 insert(Num,tree(Element,Left,Right),tree(Element,NewLeft,Right)):-Num<Element,insert(Num,Left,NewLeft).
 insert(Num,tree(Element,Left,Right),tree(Element,Left,NewRight)):-insert(Num,Right,NewRight).
 
+deleteB(Num,tree(Num,empty,empty),empty).
+deleteB(Num,tree(Num,Left,empty),Left).
+deleteB(Num,tree(Num,empty,Right),Right).
+deleteB(Num,tree(Num,Left,Right),Result):-succLeft(Right,X),deleteB(X,tree(Num,Left,Right),tree(NumNew,LeftNew,RightNew)),b(tree(X,LeftNew,RightNew),Result).
+deleteB(Num,tree(Element,L,R),Result):-Num<Element,deleteB(Num,L,ResultT),b(tree(Element,ResultT,R),Result).
+deleteB(Num,tree(Element,L,R),Result):-deleteB(Num,R,ResultT),b(tree(Element,L,ResultT),Result).
+deleteB(Num,tree(Element,L,R),tree(Element,L,R)).
+
+succLeft(tree(Num,empty,empty),Num).
+succLeft(tree(Num,L,R),X):- L \= empty,succLeft(L,X).
+succLeft(tree(Num,tree(Element,empty,_),Right),Element).
+
 
 insertB(Num,empty,tree(Num,empty,empty)):-write("insert empty").
 insertB(Num,tree(Element,Left,Right),Balanced):-Num<Element,insertB(Num,Left,NewLeft),b(tree(Element,NewLeft,Right),Balanced),write(" Insert Left: current dep: "),deep(tree(Element,NewLeft,Right),D),write(D).
 insertB(Num,tree(Element,Left,Right),Balanced):-insertB(Num,Right,NewRight),b(tree(Element,Left,NewRight),Balanced),write(" insert Right: current dep: "),deep(tree(Element,Left,NewRight),D),write(D).
 
+b(empty,empty).
 b(tree(Root,L,R),Result):-deep(R,R1),deep(L,L1),L1-R1>1,subWeight(L,Sub),Sub<0,rightLeft(tree(Root,L,R),Result),write(" right-left Rotation? "),write(L1-R1).
 b(tree(Root,L,R),Result):-deep(R,R1),deep(L,L1),L1-R1>1,rightRotation(tree(Root,L,R),Result),write(" right Rotation? "),write(L1-R1).
 
@@ -26,28 +39,9 @@ deep(tree(_,Left,Right),Length):-deep(Left,L1),deep(Right,R1), greater(L1,R1,G),
 subWeight(tree(Root,L,R),X):-deep(R,R1),deep(L,L1),X is L1-R1.
 
 
-balance(tree(Num,Left,Right),Result):-deep(Left,L),deep(Right,R),diff(L,R,Diff),Diff > 1,balanceLeft(tree(Num,Left,Right),Result).
-balance(tree(Num,Left,Right),tree(Num,NEW,Right)):-deep(Left,L),deep(Right,R),diff(L,R,Diff),Diff>2,balance(Left,NEW).
-balance(tree(Num,Left,Right),tree(Num,NEW,Right)):-deep(Left,L),deep(Right,R),diff(L,R,Diff),Diff>0,balance(Left,NEW).
-balance(tree(Num,Left,Right),tree(Num,Left,NEW)):-deep(Left,L),deep(Right,R),diff(L,R,Diff),Diff< -2,balance(Right,NEW).
-balance(tree(Num,Left,Right),Result):-deep(Left,L),deep(Right,R),diff(L,R,Diff),Diff< -1,balanceRight(tree(Num,Left,Right),Result).
-balance(tree(Num,Left,Right),tree(Num,Left,NEW)):-deep(Left,L),deep(Right,R),diff(L,R,Diff),Diff< 0,balance(Right,NEW).
-balance(In,In).
-
-orderedInsert(Num,Tree,Result):-insert(Num,Tree,ResultUnbalanced),balance(ResultUnbalanced,Result).
-
 greater(A,B,A):-A>B.
 greater(A,B,B).
 
-diff(A,B,C):- C is A - B.
-isSame(A,B):- A == B.
-same(Tree,Tree).
-
-balanceLeft(tree(M,tree(K,UL,UR),Right),Result):-deep(UL,L),deep(UR,R),diff(L,R,Diff),Diff>0,rightRotation(tree(M,tree(K,UL,UR),Right),Result).
-balanceLeft(tree(M,tree(K,UL,UR),Right),Result):-deep(UL,L),deep(UR,R),diff(L,R,Diff),Diff<0,rightLeft(tree(M,tree(K,UL,UR),Right),Result).
-
-balanceRight(tree(M,Left,tree(K,UL,UR)),Result):-deep(UL,L),deep(UR,R),diff(L,R,Diff),Diff<0,leftRotation(tree(M,Left,tree(K,UL,UR)),Result).
-balanceRight(tree(M,Left,tree(K,UL,UR)),Result):-deep(UL,L),deep(UR,R),diff(L,R,Diff),Diff>0,leftRight(tree(M,Left,tree(K,UL,UR)),Result).
 
 leftRotation(tree(X,Z,tree(K,L,M)),tree(K,tree(X,Z,L),M)).
 
